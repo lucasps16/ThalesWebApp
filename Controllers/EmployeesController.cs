9c00e9fb2b0afe90c7def8eaaade52ca579fff6c
lucasps16 <lupena@unal.edu.co>
2023-06-28 12:00:41 -0500

    Fixed Too Many Requests and 404 errors and updated MD Added Thales Logo and fixed data display

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using ThalesWebApp.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Net.Mail;
 
 namespace ThalesWebApp.Controllers
 {
@@ -24,6 +25,10 @@ namespace ThalesWebApp.Controllers
                 HttpClient client = new HttpClient();
                 HttpResponseMessage res = await client.GetAsync("https://dummy.restapiexample.com/api/v1/employees");
                 string jsonRes = await res.Content.ReadAsStringAsync();
+                if (jsonRes.StartsWith("<"))
+                {
+                    throw new Exception("Too Many Requests, try again later...");
+                }
                 dynamic data = GetDynamicObj(jsonRes);
                 foreach(dynamic item in data.data)
                 {
@@ -31,6 +36,7 @@ namespace ThalesWebApp.Controllers
                     employee.id = item.id;
                     employee.employee_name = item["employee_name"];
                     employee.employee_salary = item["employee_salary"];
+                    employee.employee_anual_salry = CalculateAnualSalary(employee.employee_salary);
                     employee.employee_age = item["employee_age"];
                     employee.profile_image = item["profile_image"];
                     employees.Add(employee);
@@ -41,7 +47,7 @@ namespace ThalesWebApp.Controllers
             catch(Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-                return new List<EmployeeModel>();
+                return null;
             }
         }
 
@@ -56,27 +62,31 @@ namespace ThalesWebApp.Controllers
                 string jsonRes = await res.Content.ReadAsStringAsync();
                 if (jsonRes.StartsWith("<"))
                 {
-                    throw new Exception();
+                    throw new Exception("Too Many Requests");
                 }
                 dynamic data = GetDynamicObj(jsonRes);
-                foreach (dynamic item in data)
+                if(data.data == null)
                 {
-                    EmployeeModel employee = new EmployeeModel();
-                    employee.id = item.id;
-                    employee.employee_name = item["employee_name"];
-                    employee.employee_salary = item["employee_salary"];
-                    employee.employee_age = item["employee_age"];
-                    employee.profile_image = item["profile_image"];
-                    employees.Add(employee);
+                    throw new HttpRequestException();
                 }
+                EmployeeModel employee = new EmployeeModel();
+                employee.id = data.data["id"];
+                employee.employee_name = data.data["employee_name"];
+                employee.employee_salary = data.data["employee_salary"];
+                employee.employee_anual_salry = CalculateAnualSalary(employee.employee_salary);
+                employee.employee_age = data.data["employee_age"];
+                employee.profile_image = data.data["profile_image"];
+                employees.Add(employee);
                 int a = 0;
                 return employees;
             }
+            catch (HttpRequestException http_ex)
+            {
+                throw http_ex;
+            }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
-                Error();
-                return new List<EmployeeModel>();
+                return null;
             }
         }
 
@@ -90,25 +100,16 @@ namespace ThalesWebApp.Controllers
                 if (id.HasValue)
                 {
                     employees = GetEmployee(id.Value).Result;
+                    if(employees == null)
+                    {
+                        return View("~/Views/Error/Index.cshtml");
+                    }
                 }
                 else
                 {
                     employees = GetEmployeeList().Result;
 
                 }
-               /* string jsonRes = await res.Content.ReadAsStringAsync();
-                dynamic data = GetDynamicObj(jsonRes);
-                foreach (dynamic item in data.data)
-                {
-                    EmployeeModel employee = new EmployeeModel();
-                    employee.id = item.id;
-                    employee.employee_name = item["employee_name"];
-                    employee.employee_salary = item["employee_salary"];
-                    employee.employee_anual_salry = CalculateAnualSalary(employee.employee_salary);
-                    employee.employee_age = item["employee_age"];
-                    employee.profile_image = item["profile_image"];
-                    employees.Add(employee);
-                }*/
 
                 EmployeeListModel model = new EmployeeListModel
                 {
@@ -120,9 +121,11 @@ namespace ThalesWebApp.Controllers
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-                return View("Index");
+                return View("~/Views/Error/Error404.cshtml");
             }
         }
+
+
 
         private dynamic GetDynamicObj(string json)
         {
@@ -141,11 +144,6 @@ namespace ThalesWebApp.Controllers
         {
             int employee_anual_salary = salary * 12;
             return employee_anual_salary;
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ThalesWebApp
+
+This .NET MVC application was build using .NET 7.0 and Visual Studio 2022.
+This application consumes data from https://dummy.restapiexample.com/api/v1/ 
+Some times this API returns a Too Many Request error, when this happens the app will redirect the user to a custom error view.
